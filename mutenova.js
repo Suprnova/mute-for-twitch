18393f8ef7d9f477e40888d646a144f7c736157f
Pixel-Dog <pixelsareawesome@hotmail.co.uk>
2020-08-09 21:21:33 +0100

    Update mutenova.js
    pretty gucci

--- mutenova.js
+++ mutenova.js
@@ -23,6 +23,7 @@
 ⠀⢀⠤⠀⠀⢤⣤⣽⣿⣿⣦⣀⢀⡠⢤⡤⠄⠀⠒⠀⠁⠀⠀⠀⢘⠔⠀⠀⠀⠀
 ⠀⠀⠀⡐⠈⠁⠈⠛⣛⠿⠟⠑⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠉⠑⠒⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀*/
+
 const tmi = require('tmi.js');
 const robot = require('robotjs');
 
@@ -30,16 +31,18 @@ const robot = require('robotjs');
 const opts = {
 	identity: {
 		username: 'mutenovabot',
-		//please remove this before uploading you retard
+		//please remove this before uploading you retard - lel
 		password: '<REDACTED>'
 	},
 	channels: [
 		'purple_foundation'
 	]
 };
+
 // Create a client with our options
 const client = new tmi.client(opts);
 client.on('connected', onConnectedHandler);
+
 // Connect to Twitch:
 client.connect();
 client.on("cheer", (channel, userstate, message) => {
@@ -52,6 +55,7 @@ client.on("cheer", (channel, userstate, message) => {
 	let bitsTime = userstate.bits * 500
 	muteNova(bitsTime);
 });
+
 client.on("subscription", (channel, username, method, message, userstate) => {
     console.log(`subscription received`)
 	
@@ -60,19 +64,40 @@ client.on("subscription", (channel, username, method, message, userstate) => {
 	client.say(channel, `Muting nova for 125 seconds`);
 	muteNova(subTime);
 });
-function muteNova(time) {
-	robot.keyToggle('control', 'down');
-	robot.keyTap('insert');
-	robot.keyToggle('control', 'up');
-	console.log('muted nova');
-	setTimeout(function () {
-		robot.keyToggle('control', 'down');
-		robot.keyTap('insert');
-		robot.keyToggle('control', 'up');
-		console.log('unmuted nova');
-	}, time);
 
+var lastMuteTimeStamp = new Date();
+var outstandingMuteTime = 0; //ms
+var timer;
+
+function muteNova(bits) {
+    var bitsToMs = bits * 500; //half second / 500ms per bit
+    msSinceLastMute = Math.abs(Date.now() - lastMuteTimeStamp);
+    lastMuteTimeStamp = Date.now();
+
+    if (msSinceLastMute < outstandingMuteTime) {
+        //outstanding mute time hasn't been run down yet; this means that we need to clear the old timeout and run a new one
+        outstandingMuteTime = outstandingMuteTime - msSinceLastMute + bitsToMs;
+        console.log(`nova mute extended to ${outstandingMuteTime / 1000} seconds`);
+
+        clearTimeout(timer);
+    } else {
+        //no outstanding mute, start a new timeout and trigger a mute
+        outstandingMuteTime = bitsToMs;
+
+        robot.keyToggle('control', 'down');
+        robot.keyTap('home');
+        robot.keyToggle('control', 'up');
+        console.log(`muted nova for ${outstandingMuteTime / 1000} seconds`);
+    }
+
+    timer = setTimeout(function () {
+        robot.keyToggle('control', 'down');
+        robot.keyTap('home');
+        robot.keyToggle('control', 'up');
+        console.log('unmuted nova');
+    }, outstandingMuteTime);
 }
+
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
 	console.log(`* Connected to ${addr}:${port}`);

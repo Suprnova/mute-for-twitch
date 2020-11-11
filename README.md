34269f2b6cb78e931af5c33edb5c42c5edb4bad8
Nova <s1rbakas@gmail.com>
2020-11-11 17:15:44 -0500

    Relocate user variables, more events
    Adds event handling for sending a message when connected and for handling resubs.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mute-for-twitch
-Console application that will mute the host's mic for a certain amount of time based on how much money they receive from their Twitch stream. Features support for Bits, Subs, and Gift Subs, customizable keybinds, and stacking durations.
+A console application that will mute the host's mic for a certain amount of time based on how much money they receive from their Twitch stream. Features support for Bits, Subs, and Gift Subs, customizable keybinds, and stacking durations.
 # Usage
 1. Download and unzip files.
 2. Install [node.js](https://nodejs.org/en/) and npm. (npm should be preinstalled from node.js.)
@@ -8,13 +8,26 @@ Console application that will mute the host's mic for a certain amount of time b
   - npm i --global --production windows-build-tools
 4. Open a non-admin Command Prompt window and type "npm i robotjs".
 5. Open mutehost.js in a text editor and make the following edits:
-  - Change the username on line 14 to the account you're using for the bot.
-  - Change the password on line 16 to your oauth token from [this link](https://twitchapps.com/tmi/).
-  - Change the channels on line 19 to the channel name that the bot will be in chat on.
-  - (Optional) Change the multipliers from lines 7-9 to your desired value. These values are how many seconds a specific action will mute you for.
+  - Replace **\<username>**, **\<password>**, and **\<channel(s)>** on lines 3, 4, and 5 with the username of the bot, the OAuth token of the bot from [this link](https://twitchapps.com/tmi/), and each channel that the bot will connect to. (This information is stored in an array, if you want to connect to multiple channels, surround each channel name with **'** and separate each one with ", ".)
+  - Replace the values on lines 6, 7, and 8 to your desired values, if preferred.
+  - Change the keybinds on line 80 and line 87 to match the keybind used for muting on Discord, OBS, etc.. (The current parameters will use the keybind **CTRL + INSERT**, visit [this link](http://robotjs.io/docs/syntax#keys) for more information on how to properly configure it.
   
-Your text should look something like this: ![Demo](https://cdn.discordapp.com/attachments/603730493074046978/743521744127787088/uh_uh_ye.png)
+Your text should look something like this:
+``` node
+var user = 'mutenovabot';
+var pass = 'oauth:****************';
+var channels = ['purple_foundation', 'suprnova', 'itspepperpot'];
+var bitMultiplier = 0.5; // 0.5 seconds per bit
+var subMultiplier = 125; // 125 seconds per sub
+var giftSubMultiplier = 125; // 125 seconds per gift sub
 
-6. Navigate to the folder you unzipped the files to and type "node mutehost.js".
+...
+
+	robot.keyToggle('alt', 'down');
+	robot.keyTap('g');
+	robot.keyToggle('alt', 'up');
+	// Keybind is ALT + G
+```
+6. Navigate to the folder you unzipped the files to in Command Prompt and type "node mutehost.js".
 
 Congrats! The bot is now up and running. You'll have to do Step 6 every time you want to have the bot go online.
--- mutehost.js
+++ mutehost.js
@@ -1,31 +1,31 @@
-/* created by twitch.tv/suprnova, thanks to ItsPepperPot and Pixel-Dog on github for code edits and optimizations, thanks to the tmi.js team for their API. */
-
-const tmi = require('tmi.js');
-const robot = require('robotjs');
-
-// Initialise variables, edit to desired amounts
+/* created by twitch.tv/suprnova, thanks to ItsPepperPot and Pixel-Dog on GitHub for code edits and optimizations, thanks to the tmi.js team for their API. */
+/* basic configuration options begin, see README.md for more information. */
+var user = '<username>';
+var pass = '<password>';
+var channels = ['<channel(s)'];
 var bitMultiplier = 0.5; // 0.5 seconds per bit
 var subMultiplier = 125; // 125 seconds per sub
 var giftSubMultiplier = 125; // 125 seconds per gift sub
+/* configuration options end */
 
-// Define configuration options
+const tmi = require('tmi.js');
+const robot = require('robotjs');
 const opts = {
 	identity: {
-		username: '<username>',
+		username: user,
 		// don't share this token publicly vv
-		password: '<password>'
+		password: pass
 	},
-	channels: [
-		'<channel>'
-	]
+	channels: channels
 };
-//^^if you edit any code above this line, update README.md to reflect the accurate line number^^
-// Create a client with our options
 const client = new tmi.client(opts);
-client.on('connected', onConnectedHandler);
 
-// Connect to Twitch:
+client.on('connected', onConnectedHandler => {
+	client.say(channels[0], `Connected to chat.`);
+});
+
 client.connect();
+
 client.on("cheer", (channel, userstate, message) => {
 	//changes between bits and bit when appropriate
 	console.log(`cheer received for ${userstate.bits} bit${userstate.bits === 1 ? '' : 's'}`);
@@ -38,6 +38,12 @@ client.on("cheer", (channel, userstate, message) => {
 
 client.on("subscription", (channel, username, method, message, userstate) => {
 	console.log(`subscription received`)
+	client.say(channel, `Muting for ${subMultiplier} seconds`);
+	muteHost(subMultiplier);
+});
+
+client.on("resub", (channel, username, months, message, userstate, methods) => {
+	console.log(`resub received`)
 	client.say(channel, `Muting for ${subMultiplier} seconds`);
 	muteHost(subMultiplier);
 });
@@ -60,35 +66,32 @@ var timer;
 
 function muteHost(time) {
 	let timeMS = time * 1000
+	msSinceLastMute = Math.abs(Date.now() - lastMuteTimeStamp);
+	lastMuteTimeStamp = Date.now();
+	if (msSinceLastMute < outstandingMuteTime) {
+		//outstanding mute time hasn't been run down yet; this means that we need to clear the old timeout and run a new one
+		outstandingMuteTime = outstandingMuteTime - msSinceLastMute + timeMS;
+		console.log(`mute extended to ${outstandingMuteTime / 1000} seconds`);
+		clearTimeout(timer);
+	} else {
+		//no outstanding mute, start a new timeout and trigger a mute
+		outstandingMuteTime = timeMS;
+		//change these to the keybind you use to mute, see http://robotjs.io/docs/syntax#keys for more help.
+		robot.keyToggle('control', 'down');
+		robot.keyTap('insert');
+		robot.keyToggle('control', 'up');
+		console.log(`muted host for ${outstandingMuteTime / 1000} seconds`);
+	}
 
-    msSinceLastMute = Math.abs(Date.now() - lastMuteTimeStamp);
-    lastMuteTimeStamp = Date.now();
-
-    if (msSinceLastMute < outstandingMuteTime) {
-        //outstanding mute time hasn't been run down yet; this means that we need to clear the old timeout and run a new one
-        outstandingMuteTime = outstandingMuteTime - msSinceLastMute + timeMS;
-        console.log(`mute extended to ${outstandingMuteTime / 1000} seconds`);
-
-        clearTimeout(timer);
-    } else {
-        //no outstanding mute, start a new timeout and trigger a mute
-        outstandingMuteTime = timeMS;
-	//change these to the keybind you use to mute, see http://robotjs.io/docs/syntax#keys for more help.
-        robot.keyToggle('control', 'down');
-        robot.keyTap('insert');
-        robot.keyToggle('control', 'up');
-        console.log(`muted host for ${outstandingMuteTime / 1000} seconds`);
-    }
-
-    timer = setTimeout(function () {
-        robot.keyToggle('control', 'down');
-        robot.keyTap('insert');
-        robot.keyToggle('control', 'up');
-        console.log('unmuted host');
-    }, outstandingMuteTime);
+	timer = setTimeout(function () {
+		robot.keyToggle('control', 'down');
+		robot.keyTap('insert');
+		robot.keyToggle('control', 'up');
+		console.log('unmuted host');
+	}, outstandingMuteTime);
 }
 
-// Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
 	console.log(`* Connected to ${addr}:${port}`);
 }
+/* Note to contributors, update the README.md line numbers to reflect any changes you've made for each commit. */
